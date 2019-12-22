e98b552f35ab1cd07d3c45e6bf4250f2f2a95758
hee20192 <html20192@daum.net>
2019-12-22 16:14:53 +0900

    마무리

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { User } = require("../models");
+const { isLogin, isLogout } = require("../passport/auth");
 
+router.post("/follow", async (req, res, next) => {
+	try {
+		const user = await User.findOne({
+			where: {
+				id: req.body.userid
+			}
+		});
+		const result = await user.addFollowing(Number(req.body.follerid));
+		res.json(result);
+	}
+	catch(err) {
+		next(err);
+	}
+});
 
 module.exports = router;
